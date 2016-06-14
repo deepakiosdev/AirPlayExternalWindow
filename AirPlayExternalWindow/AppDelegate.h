c1a8cc8c5087729112af36a67ebb3ce11f21bd98
dipak <dipak@pftblr-imac-004.local>
2016-06-14 14:23:13 +0530

    Initial Commit

---
+++ AirPlayExternalWindow/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  AirPlayExternalWindow
+//
+//  Created by dipak on 6/14/16.
+//  Copyright © 2016 Dipak. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ AirPlayExternalWindow/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  AirPlayExternalWindow
+//
+//  Created by dipak on 6/14/16.
+//  Copyright © 2016 Dipak. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
